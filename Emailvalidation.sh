b215d9632b0ea40c1582aba2f2f4de8942ac727b
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-17 08:47:52 +0530

    UC2

--- Emailvalidation.sh
+++ Emailvalidation.sh
@@ -2,11 +2,11 @@
 
 echo "Welcome to Email Pattern:"
 
-read -p "Enter the Email:" email
+read -p "Enter the Email:" Email
 
 emailpat="^[a-z]{3}[.][a-z]{3}+[@][a-z]+[.][a-z]{2}[.][a-z]{2}$"
 
-if [[ $email =~ $eamilpat ]]
+if [[ $Email =~ $eamilpat ]]
 then
 	echo "Valid Email"
 else
